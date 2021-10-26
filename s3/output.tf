882fd400b0cc89c0792445a7f49a3809ceb27dc2
aramirol <angel.ramirolatorre@gmail.com>
2021-10-26 18:37:37 +0200

    refactoring ec2 module

--- s3/output.tf
+++
@@ -1,9 +0,0 @@
-output "backet_name" {
-  description = "Bucket name"
-  value       = "${aws_s3_bucket.bucket_test.*.name}"
-}
-
-output "backet_acl" {
-  description = "Bucket ACL"
-  value       = "${aws_s3_bucket.bucket_test.*.acl}"
-}
