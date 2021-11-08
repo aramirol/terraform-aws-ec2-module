69bf10cd03b491ab5c2151b7768109962f3ac586
aramirol <angel.ramirolatorre@gmail.com>
2021-11-08 12:37:11 +0100

    testing outputs

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "instances_ip" {
   description = "Instance IP"
-  value       = "${aws_instance.*.public_ip}"
+  value       = "${aws_instance.instance_test_basic.*.public_ip}"
 }
 
-#output "instances_type" {
-#  description = "Instance Type "
-#  value       = "${aws_instance.instance_test.*.instance_type}"
-#}
+output "instances_type" {
+  description = "Instance Type "
+  value       = "${aws_instance.instance_test_basic.*.instance_type}"
+}
 
-#output "instances_id" {
-#  description = "Instance ID "
-#  value       = "${aws_instance.instance_test.*.id}"
-#}
+output "instances_id" {
+  description = "Instance ID "
+  value       = "${aws_instance.instance_test_basic.*.id}"
+}
