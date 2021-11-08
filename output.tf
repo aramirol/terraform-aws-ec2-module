736176f1763792c8ee3a9d0f9b4a49c928af6397
aramirol <angel.ramirolatorre@gmail.com>
2021-11-08 12:54:35 +0100

    disabling outputs

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-output "instances_ip" {
-  description = "Instance IP"
-  value       = "${aws_instance.instance_test_basic.*.public_ip}"
-}
+#output "instances_ip" {
+#  description = "Instance IP"
+#  value       = "${aws_instance.instance_test_basic.*.public_ip}"
+#}
 
-output "instances_type" {
-  description = "Instance Type "
-  value       = "${aws_instance.instance_test_basic.*.instance_type}"
-}
+#output "instances_type" {
+#  description = "Instance Type "
+#  value       = "${aws_instance.instance_test_basic.*.instance_type}"
+#}
 
-output "instances_id" {
-  description = "Instance ID "
-  value       = "${aws_instance.instance_test_basic.*.id}"
-}
+#output "instances_id" {
+#  description = "Instance ID "
+#  value       = "${aws_instance.instance_test_basic.*.id}"
+#}
