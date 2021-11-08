a0a63e822cf92f336291989b0487182df5439290
aramirol <angel.ramirolatorre@gmail.com>
2021-11-08 11:41:58 +0100

    adding ec2_create var

--- vars.tf
+++ vars.tf
@@ -19,3 +19,9 @@ variable "instance_ami" {
   description = "Amazon Linux instance"
   default = "ami-058e6df85cfc7760b" # eu-central-1
 }
+
+variable "ec2_create" {
+  description = "Conditional Var"
+  type = list
+  default = []
+}
