9eb649dd44a0756778b26fdd3e824e9131e45b3a
aramirol <angel.ramirolatorre@gmail.com>
2021-11-08 15:00:19 +0100

    testing outputs

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "instances_ip" {
   description = "Instance IP"
-  value       = "${aws_instance.instance_test_basic.*.public_ip}"
+  value       = "${aws_instance.instance_basic.*.public_ip}"
 }
 
 output "instances_type" {
   description = "Instance Type "
-  value       = "${aws_instance.instance_test_pro.*.instance_type}"
+  value       = "${aws_instance.instance_pro.*.instance_type}"
 }
 
 output "instances_id" {
   description = "Instance ID "
-  value       = aws_instance.instance_test_basic.*.id
+  value       = aws_instance.${var.ec2_create}.*.id
 }
