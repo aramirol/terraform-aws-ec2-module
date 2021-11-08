9033b5a52bae6205a12079745e9ce0cb992bde1a
aramirol <angel.ramirolatorre@gmail.com>
2021-11-08 13:01:04 +0100

    deleting vars

--- vars.tf
+++ vars.tf
@@ -1,10 +1,5 @@
 # EC2 vars
 
-variable "instance_count" {
-  description = "Number of instances"
-  default = "1"
-}
-
 variable "instance_type_basic" {
   description = "Type of instances"
   default = "t2.nano"
@@ -15,13 +10,13 @@ variable "instance_type_pro" {
   default = "t2.micro"
 }
 
-variable "instance_ami" {
-  description = "Amazon Linux instance"
-  default = "ami-058e6df85cfc7760b" # eu-central-1
-}
-
 variable "ec2_create" {
   description = "Conditional Var"
   type = list
   default = []
+}
+
+variable "instance_ami" {
+  description = "Amazon Linux instance"
+  default = "ami-058e6df85cfc7760b" # eu-central-1
 }
