e88adb071d87018c8353010d0ead11cf57746034
aramirol <angel.ramirolatorre@gmail.com>
2021-11-08 11:37:02 +0100

    modify instance resource name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # EC2 resource
 
-resource "aws_instance" "instance_test" {
+resource "aws_instance" "instance_test_basic" {
   count         = contains(var.ec2_create, "instance_basic") != true ? 0 : 1
   ami           = var.instance_ami
   instance_type = var.instance_type_basic
@@ -11,7 +11,7 @@ resource "aws_instance" "instance_test" {
   }
 }
 
-resource "aws_instance" "instance_test" {
+resource "aws_instance" "instance_test_pro" {
   count         = contains(var.ec2_create, "instance_pro") != true ? 0 : 1
   ami           = var.instance_ami
   instance_type = var.instance_type_pro
