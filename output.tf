9436b24a4e38945e47208cc10670173ae1c2427b
aramirol <angel.ramirolatorre@gmail.com>
2021-11-08 15:04:04 +0100

    testing outputs

--- output.tf
+++ output.tf
@@ -1,33 +1,33 @@
 # Output instances BASIC
 
-output "instances_ip" {
+output "instances_ip_basic" {
   description = "Instance IP"
   value       = "${aws_instance.instance_basic.*.public_ip}"
 }
 
-output "instances_type" {
+output "instances_type_basic" {
   description = "Instance Type "
   value       = "${aws_instance.instance_basic.*.instance_type}"
 }
 
-output "instances_id" {
+output "instances_id_basic" {
   description = "Instance ID "
   value       = "${aws_instance.instance_basic.*.id}"
 }
 
 # Output instances PRO
 
-output "instances_ip" {
+output "instances_ip_pro" {
   description = "Instance IP"
   value       = "${aws_instance.instance_pro.*.public_ip}"
 }
 
-output "instances_type" {
+output "instances_type_pro" {
   description = "Instance Type "
   value       = "${aws_instance.instance_pro.*.instance_type}"
 }
 
-output "instances_id" {
+output "instances_id_pro" {
   description = "Instance ID "
   value       = "${aws_instance.instance_pro.*.id}"
 }
