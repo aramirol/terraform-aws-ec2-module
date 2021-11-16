b112ce4bfcbcc8d85c9a2241a7a503378e5c264b
aramirol <angel.ramirolatorre@gmail.com>
2021-11-16 17:46:26 +0100

    fixed typos

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 # Output instances BASIC
 
-output "instances_ip_basic" {
-  description = "Instance IP Basic"
+output "instances_ip" {
+  description = "Instance IP"
   value       = contains(var.ec2_create, "yes") == true ? aws_instance.instance_ec2.*.public_ip : null
 }
 
-output "instances_type_basic" {
-  description = "Instance Type Basic"
+output "instances_type" {
+  description = "Instance Type"
   value       = contains(var.ec2_create, "yes") == true ? aws_instance.instance_ec2.*.instance_type : null
 }
 
-output "instances_id_basic" {
-  description = "Instance ID Basic"
+output "instances_id" {
+  description = "Instance ID"
   value       = contains(var.ec2_create, "yes") == true ? aws_instance.instance_ec2.*.id : null 
 }
