fbb2868d919c584bb5398b87b0c011476077f39e
aramirol <angel.ramirolatorre@gmail.com>
2021-11-18 11:25:49 +0100

    change ami value

--- vars.tf
+++ vars.tf
@@ -13,5 +13,6 @@ variable "ec2_create" {
 
 variable "instance_ami" {
   description = "Amazon Linux instance"
-  default = "ami-058e6df85cfc7760b" # eu-central-1
+#  default = "ami-058e6df85cfc7760b" # eu-central-1
+  default = "ami-083654bd07b5da81d" # us-east-1
 }
